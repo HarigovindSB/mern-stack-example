0ea0809a3c2d9bc0fcfb8c49f9e7ef7b8dbe7ec3
alokdevops-cyber <alokdevops0@gmail.com>
2026-09-23 23:56:20 +0530

    Update cypress.config.js

--- mern/client/cypress.config.js
+++ mern/client/cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'https://mern-stack-example-up0q.onrender.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
