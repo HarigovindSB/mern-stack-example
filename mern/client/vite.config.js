66efbe4e95876f0e2b00e7119586fe0f29290dda
alokdevops-cyber <alokdevops0@gmail.com>
2026-09-24 00:13:57 +0530

    updated render URL

--- mern/client/vite.config.js
+++ mern/client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/record': 'http://localhost:5050',
+      '/record': 'https://mern-stack-example-up0q.onrender.com',
     },
   },
 })
